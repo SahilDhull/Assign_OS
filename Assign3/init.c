1dd39292a75cee3a5cb1b625b5d35792434b9ace
Sahil Dhull <sahildhull25121998@gmail.com>
2018-10-24 12:09:31 +0530

    latest testcase

--- Assign3/init.c
+++ Assign3/init.c
@@ -1,4 +1,5 @@
 #include<init.h>
+#include<memory.h>
 static void exit(int);
 static int main(void);
 
@@ -71,11 +72,48 @@ static void alarm(int ticks){
   _syscall1(SYSCALL_ALARM,(u32)ticks);
 }
 
+static void sleep(int ticks){
+  // write((char*)ticks,8);
+  _syscall1(SYSCALL_SLEEP,(u32)ticks);
+}
+
+static void clone(void* func_addr,void* st_addr)
+{
+  // write("func address in clone call in init = ",30);
+  // write((char*)func_addr,8);
+  _syscall2(SYSCALL_CLONE, (long)func_addr,(long)st_addr) ;
+}
+
 static void func(){
   write("System Call Handler Implemented\n",5);
   // exit(0); 
   //return 0;
 }
+
+static void f(){
+  write("-------inside pid 2--------\n",30);
+  // char * ptr = expand(10,MAP_WR);
+  // clone(&f1,ptr);
+  // write("----exit with code 2----------\n",30);
+  exit(2);
+}
+
+static void f1(){
+  write("-------inside pid 3--------\n",30);
+  // char * ptr = expand(10,MAP_WR);
+  // clone(&f1,ptr);
+  // write("----exit with code 3----------\n",30);
+  exit(3);
+}
+
+static void f2(){
+  write("-------inside pid 4--------\n",30);
+  // char * ptr = expand(10,MAP_WR);
+  // clone(&f1,ptr);
+  // write("----exit with code 5----------\n",30);
+  exit(4);
+}
+
 
 static int main()
 {
@@ -83,10 +121,21 @@ static int main()
   // signal(SIGFPE,&func);
   // write((char*)&func,8);
   // int x = 1/0;
-  signal(SIGALRM,&func);
-  alarm(4);
+  // signal(SIGALRM,&func);
+  // alarm(3);
+  // write("doing sleep in next step\n",30);
+  // sleep(5);
+  // write("okay=================\n",30);
+  char * ptr = expand(10,MAP_WR);
+  // char * ptr2 = expand(10,MAP_WR);
+  clone(&f,ptr);
+  // write("\ncoming here\n",15);
+  char * ptr1 = expand(10,MAP_WR);
+  clone(&f1,ptr1);
+  // clone(&f2,ptr2);
   // for(int i=0;i<70000;i++);
-  while(1);
+  // while(1);
   // write("reached ending-----------\n",20);
+  exit(1);
   return 0;
 }
